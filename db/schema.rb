aad9ad0b614671d9ac5cd12837e8acb5885306ac
Dmitry Sky <dmitry.sky@icloud.com>
2015-06-29 15:25:12 -0600

    category items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629204749) do
+ActiveRecord::Schema.define(version: 20150629212105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20150629204749) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
   end
+
+  create_table "category_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "category_id"
+  end
+
+  add_index "category_items", ["category_id"], name: "index_category_items_on_category_id", using: :btree
+  add_index "category_items", ["item_id"], name: "index_category_items_on_item_id", using: :btree
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -28,4 +36,6 @@ ActiveRecord::Schema.define(version: 20150629204749) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "category_items", "categories"
+  add_foreign_key "category_items", "items"
 end
