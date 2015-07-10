35f4b1e6af84dc5e6cb1c01b49fdd50899c85316
Dmitry Sky <dmitry.sky@icloud.com>
2015-07-09 22:05:35 -0600

    category unit test complete

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :category_items, dependent: :destroy
   has_many :items, through: :category_items
+
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  attr_accessor :category
 
+  before do
+    @category = Category.create!(name: "Fruits")
+  end
+
+  it 'is valid' do
+    expect(category).to be_valid
+  end
+
+  it 'must have a name' do
+    category.name = nil
+    expect(category).to_not be_valid
+  end
+
+  it 'cannot have an empty string for a name' do
+    category.name = ""
+    expect(category).to_not be_valid
+  end
+
+  it 'must have a unique name' do
+    2.times { Category.create(name: "Fruits") }
+    categorys = Category.where(name: "Fruits")
+    expect(categorys.size).to eq(1)
+  end
 end
