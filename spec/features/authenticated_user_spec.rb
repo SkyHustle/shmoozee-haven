b8305e43ac9b08b6a4f198310c2a5d9e6656cf3c
Dmitry Sky <dmitry.sky@icloud.com>
2015-07-12 11:36:35 -0600

    registered user is able to login

--- spec/features/authenticated_user_spec.rb
+++ spec/features/authenticated_user_spec.rb
@@ -1,12 +1,11 @@
 require "rails_helper"
 
 RSpec.feature "the authenticated user" do
-  xscenario "registered user can login" do
+  scenario "registered user can login" do
     user = User.create(username: "dmitry", password: "kicks ass", email_address: "dmitryiscool@address.com")
 
     visit login_path
 
-    fill_in "E-mail", with: "dmitryiscool@address.com"
     fill_in "Username", with: "dmitry"
     fill_in "Password", with: "kicks ass"
     click_button "Login"
