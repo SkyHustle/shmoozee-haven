e8f2aaf33ad44419ee6ebdb2019ee1228fb996f0
Dmitry Sky <dmitry.sky@icloud.com>
2015-07-15 10:11:38 -0600

    set relationships between user, order, order_item, and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ActiveRecord::Base
   has_many :category_items
   has_many :categories, through: :category_items
+  has_many :order_items
+  has_many :orders, through: :order_items
 
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
