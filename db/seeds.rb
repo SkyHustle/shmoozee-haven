c0e95ea07eae4d5e438c987877424b4723eb7c13
Dmitry Sky <dmitry.sky@icloud.com>
2015-08-18 19:54:59 -0600

    adjust seed file, add scroll-feature to card-sections

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,9 @@ class Seed
 
   def create_items
     create_categories
-    50.times do
+    20.times do
       item = Item.new(random_item)
-      item.categories << Category.all.sample
+      item.categories << Category.first
       item.save
     end
     puts "#{Item.all.map(&:title).join(', ')} created."
@@ -33,45 +33,20 @@ class Seed
 
   def categories
     [
-      "Fruit",
-      "Vegitable",
-      "Mix"
+      "Fruit Baskets",
     ]
   end
 
   def random_item
     [
-      {title: "Purely Fruit", description: "wholesome", price: 1.50},
-      {title: "Apple", description: "nutritious and sweet", price: 3.50},
-      {title: "Grapefruit", description: "healthy and sour", price: 2.00},
-      {title: "Banana", description: "fatty and yellow", price: 14.00},
-      {title: "Salad", description: "vegetarian", price: 15.00},
-      {title: "Chocolate", description: "sweet", price: 6.99},
-      {title: "Coffee", description: "energizing", price: 3.75},
-      {title: "Tea", description: "soothing", price: 8},
-      {title: "Pasta", description: "classic", price: 2.00},
-      {title: "Cheese", description: "mmmmm", price: 9},
-      {title: "Bread", description: "wholesome", price: 4.25},
-      {title: "Peanut Butter",  description: "protein-packed", price: 52},
-      {title: "Caviar", description: "super expensive", price: 6},
-      {title: "Jam", description: "sweet!", price: 2},
-      {title: "Jelly", description: "sweeter!", price: 12},
-      {title: "Preserves", description: "fruity", price: 23},
-      {title: "Smoothie", description: "delish", price: 13},
-      {title: "Carrots", description: "full of fiber", price: 6},
-      {title: "Cereal", description: "perfect for breakfast", price: 86},
-      {title: "Blackberries", description: "my favorite!", price: 5},
-      {title: "cupcake", description: "mmmmm", price: 9},
-      {title: "muffin", description: "wholesome", price: 4.25},
-      {title: "Almond Butter",  description: "protein-packed", price: 52},
-      {title: "cookie dogh", description: "super expensive", price: 6},
-      {title: "Jamsky", description: "sweet!", price: 2},
-      {title: "Jelly welly", description: "sweeter!", price: 12},
-      {title: "Panorama", description: "fruity", price: 23},
-      {title: "table", description: "delish", price: 13},
-      {title: "squash", description: "full of fiber", price: 6},
-      {title: "oats", description: "perfect for breakfast", price: 86},
-      {title: "fresas", description: "my favorite!", price: 5}
+      {title: "Purely Fruit", description: "Complete with crisp apples, juicy oranges, and sweet pears, this sophisticated gift is guaranteed to please.", price: 39.99},
+      {title: "Orchard's Abundance", description: "Complete with Vermont cheddar cheese, premium Virginia peanuts, and handcrafted popcorn, this classic arrangement is perfect for any occasion.", price: 79.99},
+      {title: "Estate Collection", description: "Complete with over 20 tempting treats, including chocolate covered fruit, classic Vermont cheddar, and crunchy classic cookies, this sophisticated gift is guaranteed to delight no matter the occasion.", price: 149.99},
+      {title: "Orchard's Treats", description: "Complete with a robust farmhouse cheddar, hearty hors d'oeuvre crackers, and delicate wafer cookies, this timeless gift offers a variety of treats that are guaranteed to please.", price: 39.99},
+      {title: "Nature's Picnic", description: "Among our crisp apples, juicy pears, and vibrant oranges, this exquisite basket boasts an assortment of gourmet treats, like robust cheddar cheese and tender summer sausage, which all come together in this artful arrangement that's perfect for any occasion.", price: 39.99},
+      {title: "Boutiful Harvest", description: "Irresistible snacks, like classic farmhouse cheddar, handcrafted popcorn, and a hearty trail mix, this generous gift is perfect for any occasion!", price: 99.99},
+      {title: "Dipping Delight", description: "Inside, you'll find an assortment of ultra-premium fruits accompanied by a high quality fondue warmer, super delicious chocolate dipping sauce complete with tea light and forks. Guaranteed to impress, the Fondue Delight Gift Basket is a gourmet gift basket they are sure to love!", price: 59.99},
+      {title: "Signature Series", description: "Inside, you'll find an assortment of ultra-premium fruits accompanied by a high quality fondue warmer, super delicious chocolate dipping sauce complete with tea light and forks. Guaranteed to impress, the Fondue Delight Gift Basket is a gourmet gift basket they are sure to love!", price: 229.99}
     ].sample
   end
 end
