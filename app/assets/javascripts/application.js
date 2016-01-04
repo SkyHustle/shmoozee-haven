c629170b798ffdd4a75c5de380a7de7600db77d3
Dmitry Sky <dmitry.sky@icloud.com>
2016-01-04 13:30:53 -0700

    JS for disabling tool-tip for touch screens

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,18 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+$(document).foundation({
+  tooltip: {
+    selector : '.has-tip',
+    additional_inheritable_classes : [],
+    tooltip_class : '.tooltip',
+    touch_close_text: 'tap to close',
+    disable_for_touch: true,
+    tip_template : function (selector, content) {
+      return '<span data-selector="' + selector + '" class="'
+        + Foundation.libs.tooltip.settings.tooltip_class.substring(1)
+        + '">' + content + '<span class="nub"></span></span>';
+    }
+  }
+});
