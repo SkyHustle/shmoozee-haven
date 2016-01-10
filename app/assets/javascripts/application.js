17c59a19abd10d4604e6bfe87fe29d137993d8c9
Dmitry Sky <dmitry.sky@icloud.com>
2016-01-10 09:27:42 -0700

    move tooltip JS out of manifest file, update Admin header image url, add right margin to order link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,20 +14,3 @@
 //= require jquery_ujs
 //= require foundation
 //= require_tree .
-
-$(function(){ $(document).foundation(); });
-
-$(document).foundation({
-  tooltip: {
-    selector : '.has-tip',
-    additional_inheritable_classes : [],
-    tooltip_class : '.tooltip',
-    touch_close_text: 'tap to close',
-    disable_for_touch: true,
-    tip_template : function (selector, content) {
-      return '<span data-selector="' + selector + '" class="'
-        + Foundation.libs.tooltip.settings.tooltip_class.substring(1)
-        + '">' + content + '<span class="nub"></span></span>';
-    }
-  }
-});
